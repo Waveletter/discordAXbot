aeeacd8f40a26e964caaff47fb404edd405d3a7b
Waveletter <mail@mail.ru>
2023-04-09 16:13:55 +0300

    Added basic structure for personalstats command

--- reports_cog.py
+++ reports_cog.py
@@ -22,3 +22,13 @@ class ReportsCog(commands.Cog, name='Reports'):
         a = await self.parse_args(args)
         status = await self.push_to_db(a)
         await ctx.reply(f'{ctx.author.mention} доложил о закрытии {status}')
+
+    @commands.command()
+    async def personalstats(self, ctx, user=None):
+        """
+        Команда выводит статистику по указанному игроку; по умолчанию выводит статистику по вызвавшему команду
+        """
+        if user is None:
+            await ctx.reply(f"Статистика {ctx.author.mention}")
+        else:
+            await ctx.reply(f"Статистика {user}")
