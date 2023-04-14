db995e4264f6e2412424d3126fc930fe21a5f9ae
Waveletter <aaa>
2023-04-14 14:22:43 +0300

    Added config example file, added more command scaffold to general category: builds, preengineered, macros, graphic

--- general_cog.py
+++ general_cog.py
@@ -12,3 +12,32 @@ class GeneralCog(commands.Cog, name="General"):
         Проверка доступности бота
         """
         await ctx.reply(f'Pong :ping_pong:')
+
+    @commands.command()
+    async def builds(self, ctx, category):
+        """
+        Показать сборки кораблей
+        """
+        pass
+
+    @commands.command()
+    async def preengineered(self, ctx):
+        """
+        Показать информацию о предмодифицированных модулях
+        """
+        pass
+
+    @commands.command()
+    async def macros(self, ctx):
+        """
+        Набор макросов
+        """
+        pass
+
+    @commands.command()
+    async def graphic(self, ctx, selection):
+        """
+        Вывести интересующий график
+        """
+        pass
+
