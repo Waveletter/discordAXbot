f4547491f3bf6b88714ff75083edeb7a72696793
Waveletter <aaa>
2023-04-19 00:14:00 +0300

    Moved builds command from text command to an application command Added more verbose output to sync command Added bot class with inits Added report_zone modal

--- general_cog.py
+++ general_cog.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 import bot_config
 from colorama import Fore
 
+
 class GeneralCog(commands.Cog, name="General"):
 
     def __init__(self, bot):
@@ -16,54 +17,7 @@ class GeneralCog(commands.Cog, name="General"):
         await ctx.reply(f'Pong :ping_pong:')
 
     @commands.command(name='builds')
-    async def builds(self, ctx, *args):
-        """
-        Показать сборки кораблей. Разрешены категории 'ax', 'trade', 'pvp', 'pve', 'explore'
-        """
-        allowed_cats = ['ax', 'trade', 'pvp', 'pve', 'explore', 'all']
-        category_description = {'ax': 'Сборки для противодействия таргоидам',
-                                'trade': 'Сборки для торговых кораблей',
-                                'pvp': 'Сборки для противодействия игрокам',
-                                'pve': 'Сборки для противодействия NPC',
-                                'explore': 'Сборки для исследования галактики'
-                                }
-        categories = list()
-        if args.count('all') > 0:
-            allowed_cats.pop(allowed_cats.index('all'))
-            categories = allowed_cats
-        elif len(args) == 0:
-            categories.append('ax')
-        else:
-            for category in args:
-                try:
-                    categories.append(allowed_cats[allowed_cats.index(category.lower())])
-                except ValueError:
-                    await ctx.reply(f'Категории {category} не существует. Разрешённые категории: {allowed_cats}')
-                    return
 
-        reply = dict()
-        for i in categories:
-            reply.update({i: list()})
-
-        try:
-            with open(bot_config.settings['builds'], 'r') as builds:
-                for line in builds:
-                    for cat in reply.keys():
-                        if line.startswith(cat):
-                            reply[cat].append(
-                                (line.strip().split(';'))[1:])  # append a pair of build name and build link
-        except Exception as ex:
-            await ctx.reply(f'File error {ex}')
-            return
-
-        embed = discord.Embed(title='Сборки кораблей по категориям', color=discord.Color.dark_gold(),
-                              description=f'Доступные категории: {allowed_cats}')
-        for category in reply.keys():
-            embed.add_field(name=f'{category.upper():-^40}', value=category_description[category], inline=False)
-            for record in reply[category]:
-                embed.add_field(name=record[0], value=f'[Link]({record[1]})', inline=False)
-
-        await ctx.reply(embed=embed)
 
     #@commands.hybrid_command()
     async def preengineered(self, ctx):
@@ -86,12 +40,25 @@ class GeneralCog(commands.Cog, name="General"):
         """
         pass
 
-    @commands.hybrid_command()
+    @commands.command(hidden=True)
     @commands.is_owner()
     async def sync(self, ctx):
-        #TODO: разобраться почему не распознаёт команды для синхронизации
-        """Синхронизирует команды с сервером"""
+        #TODO: разобраться почему не распознаёт команды для синхронизации с сервером
+        """Синхронизирует команды с текущим сервером"""
         print(f'Started syncing on {ctx.guild}')
         synced = await self.bot.tree.sync(guild=ctx.guild)
         print(f'{Fore.YELLOW + str(len(synced))} commands have been synced with {ctx.guild}')
+        for cmd in synced:
+            print(f'{Fore.CYAN + str(cmd) + Fore.RESET}')
+        await ctx.reply('Sync complete')
+
+    @commands.command(hidden=True)
+    @commands.is_owner()
+    async def global_sync(self, ctx):
+        """Синхронизирует команды глобально"""
+        print(f'Started global syncing from command on {ctx.guild}')
+        synced = await self.bot.tree.sync()
+        print(f'{Fore.YELLOW + str(len(synced))} commands have been synced as global')
+        for cmd in synced:
+            print(f'{Fore.CYAN + str(cmd) + Fore.RESET}')
         await ctx.reply('Sync complete')
